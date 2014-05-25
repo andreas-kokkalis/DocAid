a1408620188d70c65e6298fd8aaf193dff840dfc
Andreas Kokkalis <a.kokkalis@kth.se>
2014-05-25 18:13:32 +0200

    minor local changes

--- src/se/kth/ict/docaid/course/CourseCodeParser.java
+++ src/se/kth/ict/docaid/course/CourseCodeParser.java
@@ -15,61 +15,69 @@ public class CourseCodeParser {
 
 	private static ArrayList<Course> retrieveCourseCodes(Document doc) {
 		ArrayList<Course> courses = new ArrayList<Course>();
-			doc.getDocumentElement().normalize();
-			System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
-			NodeList nList = doc.getElementsByTagName("courseRound");
-			System.out.println("----------------------------");
+		doc.getDocumentElement().normalize();
+		System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+		NodeList nList = doc.getElementsByTagName("courseRound");
+		System.out.println("----------------------------");
 
-			for (int temp = 0; temp < nList.getLength(); temp++) {
-				Node nNode = nList.item(temp);
-				// System.out.println("\nCurrent Element :" + nNode.getNodeName());
-				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-					Element eElement = (Element) nNode;
-					Course course = new Course();
-					course.setCode(eElement.getAttribute("courseCode"));
-					course.setRound(eElement.getAttribute("roundId"));
-					courses.add(course);
-				}
+		for (int temp = 0; temp < nList.getLength(); temp++) {
+			Node nNode = nList.item(temp);
+			// System.out.println("\nCurrent Element :" + nNode.getNodeName());
+			if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+				Element eElement = (Element) nNode;
+				Course course = new Course();
+				course.setCode(eElement.getAttribute("courseCode"));
+				course.setRound(eElement.getAttribute("roundId"));
+				courses.add(course);
 			}
-			return courses;
+		}
+		return courses;
 	}
 
-//	private static void printNote(NodeList nodeList) {
-//
-//		for (int count = 0; count < nodeList.getLength(); count++) {
-//
-//			Node tempNode = nodeList.item(count);
-//			// make sure it's element node.
-//			if (tempNode.getNodeType() == Node.ELEMENT_NODE) {
-//				// get node name and value
-//				System.out.println("\nNode Name =" + tempNode.getNodeName() + " [OPEN]");
-//				System.out.println("Node Value =" + tempNode.getTextContent());
-//				if (tempNode.hasAttributes()) {
-//					// get attributes names and values
-//					NamedNodeMap nodeMap = tempNode.getAttributes();
-//					for (int i = 0; i < nodeMap.getLength(); i++) {
-//						Node node = nodeMap.item(i);
-//						System.out.println("attr name : " + node.getNodeName());
-//						System.out.println("attr value : " + node.getNodeValue());
-//					}
-//				}
-//				if (tempNode.hasChildNodes()) {
-//					// loop again if has child nodes
-//					printNote(tempNode.getChildNodes());
-//				}
-//
-//				System.out.println("Node Name =" + tempNode.getNodeName() + " [CLOSE]");
-//			}
-//		}
-//	}
+	private static void printNote(NodeList nodeList) {
+
+		for (int count = 0; count < nodeList.getLength(); count++) {
+
+			Node tempNode = nodeList.item(count);
+			// make sure it's element node.
+			if (tempNode.getNodeType() == Node.ELEMENT_NODE) {
+				// get node name and value
+				System.out.println("\nNode Name =" + tempNode.getNodeName() + " [OPEN]");
+				System.out.println("Node Value =" + tempNode.getTextContent());
+				if (tempNode.hasAttributes()) {
+					// get attributes names and values
+					NamedNodeMap nodeMap = tempNode.getAttributes();
+					for (int i = 0; i < nodeMap.getLength(); i++) {
+						Node node = nodeMap.item(i);
+						System.out.println("attr name : " + node.getNodeName());
+						System.out.println("attr value : " + node.getNodeValue());
+					}
+				}
+				if (tempNode.hasChildNodes()) {
+					// loop again if has child nodes
+					printNote(tempNode.getChildNodes());
+				}
+
+				System.out.println("Node Name =" + tempNode.getNodeName() + " [CLOSE]");
+			}
+		}
+	}
 
 	public static void main(String argv[]) {
 		try {
 			DocumentBuilder dBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-			Document doc = dBuilder.parse("http://www.kth.se/api/kopps/v1/courseRounds/2010:2");
+			Document doc = dBuilder.parse("http://www.kth.se/api/kopps/v1/courseRounds/2014:2");
+			
 			ArrayList<Course> courses = retrieveCourseCodes(doc);
-			for(Course course: courses) 
-				System.out.println("courseCode: " + course.getCode());
+			System.out.println(courses.size());
+//			for (Course course : courses)
+//				System.out.println("courseCode: " + course.getCode());
+			
+			Document doc1 = dBuilder.parse("http://www.kth.se/api/kopps/v1/courseRounds/2014:1");
+			ArrayList<Course> courses1 = retrieveCourseCodes(doc1);
+			System.out.println(courses1.size());
+//			for (Course course : courses)
+//				System.out.println("courseCode: " + course.getCode());
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
