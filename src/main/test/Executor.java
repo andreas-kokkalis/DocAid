33e14fa43f8c216b7863e454e5e745f719cb3d29
Adrian Prelipcean <adrian@adrian-laptop>
2014-05-25 18:12:14 +0200

    added jsoup html strip

--- src/main/test/Executor.java
+++ src/main/test/Executor.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.util.LinkedList;
 
 import org.apache.tika.exception.TikaException;
+import org.jsoup.Jsoup;
 import org.xml.sax.SAXException;
 
 import se.kth.ict.docaid.reader.WebReader;
@@ -23,14 +24,16 @@ public class Executor {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		String url = "http://www.kth.se/student/kurser/kurs/ID2216?startterm=20131&l=en";
+		String url = "http://www.kth.se/api/kopps/v1/course/AG2425";
 
-		WebDocument doc = new WebDocument(url);
+	/*	WebDocument doc = new WebDocument(url);
 		WebReader reader = new WebReader(doc);
-//		System.out.println(doc.toString());
+    	System.out.println(doc.toString());
 		System.out.println("\n\n=========================================================\n=========================================================");
 		System.out.println(reader.getKeywords());
-
+*/
+		
+		System.out.println(Jsoup.parse("<p>Geographic Information Systems (GIS) are being used in a wide variety of applications. A key component of any GIS application is the underlying spatial database which must be designed to support efficient data storage, access and analysis operations.This course focuses on the design and development of spatial databases. Particular emphasis will be placed on the use of data modeling techniques to design a GIS database for a specific application. Students will work in small groups to develop a conceptual design for a GIS database and will then work individually to build a spatial database using digital data available through digital library as well as data digitized from existing maps, imagery and field data collected using GPS. The resulting database will be used to perform some basic spatial analysis.</p>").text());
 		LinkedList<File> files = UtilClass.getTestFiles();
 		for (File f : files) {
 			try {
